514bd349782b28eb29bd97083f022ec6427121ba
Jorge Verdugo <jorgeverdugoch@gmail.com>
2025-08-18 15:28:12 -0400

    refactor: simplify main function by removing proxy and image blocking logic

--- main.py
+++ main.py
@@ -2,37 +2,12 @@ from camoufox.server import launch_server
 import os
 
 def main():
-    if 'PROXY_SERVER' in os.environ:
-        proxy_server = os.environ['PROXY_SERVER']
-
-    if 'PROXY_USERNAME' in os.environ:
-        proxy_username = os.environ['PROXY_USERNAME']
-    
-    if 'PROXY_PASSWORD' in os.environ:
-        proxy_password = os.environ['PROXY_PASSWORD']
-
-    if (proxy_server and proxy_username and proxy_password):
-        proxy = {
-            'server': proxy_server,
-            'username': proxy_username,
-            'password': proxy_password
-        }
-    else:
-        proxy = None
-
-    if 'BLOCK_IMAGES' in os.environ:
-        block_images = os.environ['BLOCK_IMAGES'] == 'true'
-    else:
-        block_images = True
-
     launch_server(
         headless="virtual",
-        geoip=True,
         humanize=True,
-        block_images=block_images,
-        proxy=proxy,
         port=1234,
-        ws_path='test'
+        ws_path='test',
+        geoip=True,
     )
 
 if __name__ == "__main__":
