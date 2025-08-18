3bdcc4f244f16d75d5dfb0886023290ddcf576b1
Jorge Verdugo <jorgeverdugoch@gmail.com>
2025-08-18 15:48:59 -0400

    feat: add proxy configuration support in main function

--- main.py
+++ main.py
@@ -2,12 +2,30 @@ from camoufox.server import launch_server
 import os
 
 def main():
+    if 'PROXY_SERVER' in os.environ:
+        proxy_server = os.environ['PROXY_SERVER']
+
+    if 'PROXY_USERNAME' in os.environ:
+        proxy_username = os.environ['PROXY_USERNAME']
+    
+    if 'PROXY_PASSWORD' in os.environ:
+        proxy_password = os.environ['PROXY_PASSWORD']
+
+    if (proxy_server and proxy_username and proxy_password):
+        proxy = {
+            'server': proxy_server,
+            'username': proxy_username,
+            'password': proxy_password
+        }
+    else:
+        proxy = None
+
     launch_server(
-        headless=False,
-        humanize=True,
-        port=1234,
-        ws_path='test',
         geoip=True,
+        humanize=True,
+        proxy=proxy,
+        port=1234,
+        ws_path='/',
     )
 
 if __name__ == "__main__":
